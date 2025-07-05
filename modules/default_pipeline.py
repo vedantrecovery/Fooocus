f79d6c9e4811b2caab8890f87d36881a98ba7bf5
vedantrecovery <vedantrecovery@gmail.com>
2025-07-05 18:24:17 +0000

    🔒 Locked Zoe seed at 123456 for consistent look

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -334,7 +334,7 @@ def get_candidate_vae(steps, switch, denoise=1.0, refiner_swap_method='joint'):
 @torch.no_grad()
 @torch.inference_mode()
 def process_diffusion(positive_cond, negative_cond, steps, switch, width, height, image_seed, callback, sampler_name, scheduler_name, latent=None, denoise=1.0, tiled=False, cfg_scale=7.0, refiner_swap_method='joint', disable_preview=False):
-    image_seed = 123456  # 🔒 Hardcoded seed to lock Zoe's look
+    image_seed = 5026862375781950516  # 🔒 Hardcoded seed to lock Zoe's look
     target_unet, target_vae, target_refiner_unet, target_refiner_vae, target_clip \
         = final_unet, final_vae, final_refiner_unet, final_refiner_vae, final_clip
 
